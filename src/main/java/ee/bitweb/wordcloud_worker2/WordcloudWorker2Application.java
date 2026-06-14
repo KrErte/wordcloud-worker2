3dd632d179fcbd1803b709f3f2aa68b36ccbea0c
Kristo Erte <kristo.erte@gmail.com>
2026-06-14 11:53:54 +0300

    Created new worker project

---
+++ src/main/java/ee/bitweb/wordcloud_worker2/WordcloudWorker2Application.java
@@ -0,0 +1,13 @@
+package ee.bitweb.wordcloud_worker2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordcloudWorker2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordcloudWorker2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ee/bitweb/wordcloud_worker2/WordcloudWorker2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.bitweb.wordcloud_worker2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordcloudWorker2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
